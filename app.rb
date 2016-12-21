58030742a35557659d842eb100d96f3917e9c555
github username <github email address>
2016-12-21 13:32:08 -0500

    Done.

--- app.rb
+++ app.rb
@@ -4,6 +4,23 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do
+      erb :index
+    end
+
+    get '/new' do
+      erb :"pirates/new"
+    end
+
+    post '/pirates' do
+      @pirate = params["pirate"]
+      @name = params["pirate"]["name"]
+      @ship_name_1 = params["pirate"]["ships"][0]["name"]
+      @ship_name_2 = params["pirate"]["ships"][1]["name"]
+
+      #binding.pry
+      erb :"pirates/show"
+    end
 
   end
 end
